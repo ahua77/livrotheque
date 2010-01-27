3bf6e4cd749b1beb6cdbc84867e259dfe0e1757c
fougny <fougny@42eda3aa-e831-e092-880c-b46d1eaaeeec>
2010-01-27 09:19:30 +0000

    passage en 1.2.0.0
    git-svn-id: https://svn.kenai.com/svn/livrotheque~livrotheque-repository@17 42eda3aa-e831-e092-880c-b46d1eaaeeec

--- zip_code.bat
+++ zip_code.bat
@@ -1,2 +1,2 @@
-REM D:\developpement\7zip\7z.exe a -r -t7z -m9=LZMA -x!*.exe -x!*.zip -x!*.7z -x!*.o -x!*.db code_livro
-c:\progra~1\7-Zip\7z.exe a -r -t7z -m9=LZMA -x!*.exe -x!*.zip -x!*.7z -x!*.o -x!*.db -x!secret_key.h code_livro
+D:\developpement\7zip\7z.exe a -r  -xr!?svn\* -x!*.exe -x!*.zip -x!*.7z -x!*.o -x!*.db code_livro
+rem c:\progra~1\7-Zip\7z.exe a -r -t7z -m9=LZMA -x!*.exe -x!*.zip -x!*.7z -x!*.o -x!*.db -x!secret_key.h code_livro
